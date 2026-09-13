ad8d793df6196695d75c5b947f83578574f18e75
Shahbaz Younus <52800069+shahbazm788@users.noreply.github.com>
2026-09-13 11:49:36 +0500

    Add service worker for caching static files
    Implement service worker for caching assets.

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'code-editor-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(name) {
+                    return name !== CACHE_NAME;
+                }).map(function(name) {
+                    return caches.delete(name);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
